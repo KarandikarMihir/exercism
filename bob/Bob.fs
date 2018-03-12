8194de9b8c20d3645cdbb7588ad9f9e02dd75a64
Mihir Karandikar <karandikar.mihir@outlook.com>
2018-03-12 06:10:50 +0530

    Bob: Add type, converter

--- bob/Bob.fs
+++ bob/Bob.fs
@@ -1,3 +1,22 @@
 ﻿module Bob
 
-let response (input: string): string = failwith "You need to implement this function."
+type Input = Question | Yell | YellAQuestion | SayNothing | Whatever
+
+let toString = function
+    | Question -> "Sure."
+    | Yell -> "Whoa, chill out!"
+    | YellAQuestion -> "Calm down, I know what I'm doing"
+    | SayNothing -> "Fine. Be that way"
+    | Whatever -> "Whatever"
+
+let (|Questioning|_|) (str: string) = 
+    Some Question
+
+let sanitize (x: string) = x.Trim()
+
+let response (str: string) =
+    match sanitize str with
+    | Questioning x ->
+        match x with
+        | r -> toString r
+    | _ -> toString Whatever
