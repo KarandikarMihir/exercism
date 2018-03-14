9ab85ac91f5f3bba07e9972c642bc6ba653ad1bc
Mihir Karandikar <karandikar.mihir@outlook.com>
2018-03-14 22:47:30 +0530

    Solve sum of multiples :relaxed:

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,3 +1,12 @@
 ﻿module SumOfMultiples
 
-let sum (numbers: int list) (upperBound: int): int = failwith "You need to implement this function."
+let sum set upperBound =
+    let folder acc num =
+        let rec folder' counter multiples =
+            if num >= upperBound then [] else
+            let next = num * counter
+            if next >= upperBound then multiples
+            else folder' (counter + 1) (multiples @ [next])
+        acc @ folder' 1 []
+
+    set |> ((List.fold folder []) >> List.distinct >> List.sum)
